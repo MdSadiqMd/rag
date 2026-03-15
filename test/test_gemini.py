649963b8032c054c4bced5605992edeabe9ee4ab
Md.Sadiq <mohammadsadiq4950@gmail.com>
2026-03-15 21:07:35 +0530

    testing gemini api

---
+++ test/test_gemini.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+if not api_key:
+    raise RuntimeError("GEMINI_API_KEY environment variable not set")
+
+client = genai.Client(api_key=api_key)
+model = "gemma-3-27b-it"
+
+
+def generate_content():
+    prompt = "Why is Boot.dev such a great place to learn about RAG? Use one paragraph maximum."
+    response = client.models.generate_content(model=model, contents=prompt)
+    print(response.text)
+    print(response.usage_metadata.prompt_token_count)
+    print(response.usage_metadata.candidates_token_count)
+
+
+if __name__ == "__main__":
+    generate_content()
